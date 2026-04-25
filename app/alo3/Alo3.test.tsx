7827c5a13a69ac4afbf82ec0afe0abfaab140e28
dinhvan <dinhvanle9xpro@gmail.com>
2026-04-25 15:07:26 +0700

    refactor

--- app/alo3/Alo3.test.tsx
+++ app/alo3/Alo3.test.tsx
@@ -1,9 +1,9 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Alo2 from "./page";
+import Alo3 from "./page";
 
 test("Page", () => {
-  render(<Alo2 />);
+  render(<Alo3 />);
   expect(
     screen.getByRole("heading", { level: 1, name: "Alo 2 page" }),
   ).toBeDefined();
---
+++ app/alo3/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Alo3() {
+  return (
+    <div>
+      <h1>Alo 3 page</h1>
+      <Link href="/alo3">Alo 3 page</Link>
+    </div>
+  );
+}
